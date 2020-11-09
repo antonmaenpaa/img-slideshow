7cb11f37ff243224f53f3808f851d7468a42b1af
antonmaenpaa <antonmaenpaa@gmail.com>
2020-11-09 12:08:56 +0100

    update

---
+++ main.js
@@ -0,0 +1,26 @@
+window.onload = startSlidesShow;
+
+function startSlidesShow() {
+    setInterval(doAnimationSlideShow, 3000);
+
+}
+function doAnimationSlideShow() {
+let imgs = document.querySelectorAll('img');
+
+for (let i = 0; i < imgs.length; i++) {
+
+    if(imgs[i].classList.contains('show')){
+        imgs[i].classList.remove('show')
+
+        let nextIndex = (i + 1);
+        if(nextIndex >= imgs.length -1) {
+            nextIndex = 0;
+        }
+        imgs[nextIndex].classList.add('show');
+        break;
+    } 
+    
+
+
+}
+}
